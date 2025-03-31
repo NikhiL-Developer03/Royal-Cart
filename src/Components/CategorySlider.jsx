15bc3e374f5c15822ca5fa8f0ee5f0278e95b453
Nikhil.Developer03 <nikhil.developer03@gmail.com>
2025-03-31 11:07:06 +0530

    add slider feature

--- src/Components/CategorySlider.jsx
+++ src/Components/CategorySlider.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { FreeMode, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -7,6 +7,33 @@ import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
 const CategorySlider = () => {
+  let [showPerPage, setShowPerPage] = useState(6);
+  let options = {
+    slidesPerView:showPerPage,
+          spaceBetween:50,
+          loop:true,
+          freeMode:true,
+          pagination:{
+            clickable: true,
+          },
+          modules:[FreeMode, Pagination],
+          className:"mySwiper"
+  }
+
+  function handelWindowResize() {
+    if (window.innerWidth < 576) {
+      setShowPerPage(2);
+    } else if (window.innerWidth < 768) {
+      setShowPerPage(3);
+    } else if (window.innerWidth < 992) {
+      setShowPerPage(4);
+    } else if (window.innerWidth < 1200) {
+      setShowPerPage(5);
+    } else {
+      setShowPerPage(6);
+    }
+  }
+  window.addEventListener("resize", handelWindowResize);
   return (
     <section id="clients" class="clients section">
       <div class="container section-title" data-aos="fade-up">
@@ -18,70 +45,62 @@ const CategorySlider = () => {
       </div>
 
       <div class="container" data-aos="fade-up" data-aos-delay="100">
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          freeMode={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[FreeMode, Pagination]}
-          className="mySwiper"
+        <Swiper {...options}
         >
           <SwiperSlide class="swiper-slide">
             <img
               src="assets/img/clients/client-1.png"
               class="img-fluid"
-              alt=""
+              style={{ height: 80 }}
             />
           </SwiperSlide>
           <SwiperSlide class="swiper-slide">
             <img
               src="assets/img/clients/client-2.png"
               class="img-fluid"
-              alt=""
+              style={{ height: 80 }}
             />
           </SwiperSlide>
           <SwiperSlide class="swiper-slide">
             <img
               src="assets/img/clients/client-3.png"
               class="img-fluid"
-              alt=""
+              style={{ height: 80 }}
             />
           </SwiperSlide>
           <SwiperSlide class="swiper-slide">
             <img
               src="assets/img/clients/client-4.png"
               class="img-fluid"
-              alt=""
+              style={{ height: 80 }}
             />
           </SwiperSlide>
           <SwiperSlide class="swiper-slide">
             <img
               src="assets/img/clients/client-5.png"
               class="img-fluid"
-              alt=""
+              style={{ height: 80 }}
             />
           </SwiperSlide>
           <SwiperSlide class="swiper-slide">
             <img
               src="assets/img/clients/client-6.png"
               class="img-fluid"
-              alt=""
+              style={{ height: 80 }}
             />
           </SwiperSlide>
           <SwiperSlide class="swiper-slide">
             <img
               src="assets/img/clients/client-7.png"
               class="img-fluid"
-              alt=""
+              style={{ height: 80 }}
             />
           </SwiperSlide>
           <SwiperSlide class="swiper-slide">
             <img
               src="assets/img/clients/client-8.png"
               class="img-fluid"
-              alt=""
+              style={{ height: 80 }}
             />
           </SwiperSlide>
           <div class="swiper-pagination"></div>
